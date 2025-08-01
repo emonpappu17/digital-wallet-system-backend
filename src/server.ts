65184e10d6caea5779809b2e0d36b80d4801517e
emonpappu17 <emon.pappu.5@gmail.com>
2025-08-01 14:58:14 +0600

    add handled server turning off

--- src/server.ts
+++ src/server.ts
@@ -19,3 +19,51 @@ const startServer = async () => {
 }
 
 startServer();
+
+process.on("SIGTERM", () => {
+    console.log("SIGTERM signal recieved...  Server shutting down..");
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        })
+    }
+
+    process.exit(1)
+})
+
+process.on("SIGINT", () => {
+    console.log("SIGINT signal recieved...  Server shutting down..");
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        })
+    }
+
+    process.exit(1)
+})
+
+process.on("unhandledRejection", (err) => {
+    console.log("Unhandled Rejection detected...  Server shutting down..", err);
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        })
+    }
+
+    process.exit(1)
+})
+
+process.on("uncaughtException", (err) => {
+    console.log("Unhandled Exception detected...  Server shutting down..", err);
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        })
+    }
+
+    process.exit(1)
+})
