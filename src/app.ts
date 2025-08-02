44d7cd6f4eb3b1566d99a1e2aa5b630e1cd74b1a
emonpappu17 <emon.pappu.5@gmail.com>
2025-08-02 11:52:14 +0600

    add cors setup and forntend url

--- src/app.ts
+++ src/app.ts
@@ -3,15 +3,21 @@ import { globalErrorHandler } from "./app/middlewares/globalErrorHandler"
 import notFound from "./app/middlewares/notFound"
 import { router } from "./app/routes"
 import path from "path"
+import cors from "cors"
+import { envVars } from "./app/config/env"
+
 
 const app: Application = express()
 
 app.use(express.json())
+app.use(cors({
+    origin: envVars.FRONTEND_URL,
+    credentials: true
+}))
 
 app.use("/api/v1", router)
 
 app.use(express.static(path.join(__dirname, "../public")));
-
 
 app.get("/", (req: Request, res: Response) => {
     res.sendFile(path.join(__dirname, "../public/index.html"))
--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -12,11 +12,12 @@ interface EnvConfig {
     JWT_REFRESH_SECRET: string,
     JWT_REFRESH_EXPIRES: string,
     ADMIN_PHONE_NUMBER: string,
-    ADMIN_PASSWORD: string
+    ADMIN_PASSWORD: string,
+    FRONTEND_URL: string
 }
 
 const loadEnvVariables = (): EnvConfig => {
-    const requiredVariables: string[] = ["PORT", "DB_URL", "BCRYPT_SALT_ROUND", "NODE_ENV", "JWT_ACCESS_SECRET", "JWT_ACCESS_EXPIRES", "JWT_REFRESH_SECRET", "JWT_REFRESH_EXPIRES", "ADMIN_PHONE_NUMBER", "ADMIN_PASSWORD"]
+    const requiredVariables: string[] = ["PORT", "DB_URL", "BCRYPT_SALT_ROUND", "NODE_ENV", "JWT_ACCESS_SECRET", "JWT_ACCESS_EXPIRES", "JWT_REFRESH_SECRET", "JWT_REFRESH_EXPIRES", "ADMIN_PHONE_NUMBER", "ADMIN_PASSWORD", "FRONTEND_URL"]
 
     requiredVariables.forEach(key => {
         if (!process.env[key]) {
@@ -34,7 +35,8 @@ const loadEnvVariables = (): EnvConfig => {
         JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET as string,
         JWT_REFRESH_EXPIRES: process.env.JWT_REFRESH_EXPIRES as string,
         ADMIN_PHONE_NUMBER: process.env.ADMIN_PHONE_NUMBER as string,
-        ADMIN_PASSWORD: process.env.ADMIN_PASSWORD as string
+        ADMIN_PASSWORD: process.env.ADMIN_PASSWORD as string,
+        FRONTEND_URL: process.env.FRONTEND_URL as string
     }
 }
 
