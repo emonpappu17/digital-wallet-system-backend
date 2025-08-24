97bf9a01b2fcde370490c59e16a2026e49cfe7b0
emonpappu17 <emon.pappu.5@gmail.com>
2025-08-24 13:56:11 +0600

    add:agent stats

--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -1,9 +1,10 @@
+import mongoose from "mongoose"
 import AppError from "../../errorHelpers/AppError"
 import { Transaction } from "../transaction/transaction.model"
 import { Role } from "../user/user.interface"
-import { User } from "../user/user.model"
 import { Wallet } from "../wallet.ts/wallet.model"
 import httpStatus from "http-status-codes"
+import { User } from "../user/user.model"
 
 const getAllUsers = async () => {
     const users = await User.find({ role: Role.USER }).select("-password").sort({ createdAt: -1 })
@@ -14,11 +15,99 @@ const getAllUsers = async () => {
 }
 
 const getAllAgents = async () => {
-    const agents = await User.find({ role: Role.AGENT }).select("-password").sort({ createdAt: -1 })
+    // const agents = await User.find({ role: Role.AGENT }).select("-password").sort({ createdAt: -1 })
+    // if (!agents) throw new AppError(httpStatus.NOT_FOUND, "Agents not found")
+    // return agents;
 
-    if (!agents) throw new AppError(httpStatus.NOT_FOUND, "Agents not found")
+    // const user = User.collection.name
+    const walletCollName = Wallet.collection.name;
+    const txCollName = Transaction.collection.name;
 
-    return agents;
+    const pipeline: mongoose.PipelineStage[] = [
+        // Step-1
+        { $match: { role: Role.AGENT } },
+        // Step-2
+        {
+            $lookup: {
+                from: walletCollName,
+                localField: "_id", // my agent id
+                foreignField: "user", // wallet user which is my agent id
+                as: "wallet"
+            }
+        },
+        // Step-3
+        {
+            // $unwind: "$wallet"
+            $unwind: { path: "$wallet", preserveNullAndEmptyArrays: true }
+        },
+        // Step-4
+        {
+            $lookup: {
+                from: txCollName,
+                let: { agentId: "$_id" },
+                pipeline: [
+                    {
+                        $match: {
+                            $expr: {
+                                $and: [
+                                    // only completed trans
+                                    { $eq: ["$status", "COMPLETED"] },
+                                    // agent involved if 'from' or 'to' matches agent id
+                                    {
+                                        $or: [
+                                            { $eq: ["$from", "$$agentId"] },
+                                            { $eq: ["$to", "$$agentId"] },
+                                        ]
+                                    }
+                                ]
+                            }
+                        }
+                    },
+                    { $project: { amount: 1, agentCommission: 1 } }
+                ],
+                as: "transactions",
+            }
+        },
+        // Step-5
+        {
+            $addFields: {
+                transactionsCount: { $size: { $ifNull: ["$transactions", []] } },
+                transactionVolume: {
+                    $reduce: {
+                        input: { $ifNull: ["$transactions", []] },
+                        initialValue: 0,
+                        in: { $add: ["$$value", { $ifNull: ["$$this.amount", 0] }] },
+                    }
+                },
+                commission: {
+                    $reduce: {
+                        input: { $ifNull: ["$transactions", []] },
+                        initialValue: 0,
+                        in: { $add: ["$$value", { $ifNull: ["$$this.agentCommission", 0] }] }
+
+                    }
+                },
+                balance: { $ifNull: ["$wallet.balance", 0] },
+            }
+        },
+        // Step-6
+        {
+            $project: {
+                password: 0,
+                wallet: 0,
+                transactions: 0
+            }
+        },
+        // Step-7
+        { $sort: { createAt: -1 } }
+
+    ];
+
+    const agentsWithStats = await User.aggregate(pipeline).exec();
+
+    // console.log('agentsWithStats==>', agentsWithStats); //users
+
+    return agentsWithStats;
 }
 
 const getAllWallets = async () => {
